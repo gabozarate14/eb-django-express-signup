53299d94a04f2b50a9553dbfc7e1c5565d53263a
gabozarate14 <gabozarate14@gmail.com>
2023-03-28 13:26:17 +0200

    task 4.6

--- form/models.py
+++ form/models.py
@@ -8,6 +8,7 @@ AWS_REGION = os.environ['AWS_REGION']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_SESSION_TOKEN = os.environ['AWS_SESSION_TOKEN']
+NEW_SIGNUP_TOPIC = os.environ['NEW_SIGNUP_TOPIC']
 
 
 logger = logging.getLogger(__name__)
@@ -50,3 +51,55 @@ class Leads(models.Model):
             logger.error('Unknown error inserting item to database.')
 
         return status
+
+    def send_notification(self, email):
+        sns = boto3.client('sns', region_name=AWS_REGION,
+                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                           aws_session_token=AWS_SESSION_TOKEN)
+        try:
+            sns.publish(
+                TopicArn=NEW_SIGNUP_TOPIC,
+                Message='New signup: %s' % email,
+                Subject='New signup',
+            )
+            logger.error('SNS message sent.')
+
+        except Exception as e:
+            logger.error(
+                'Error sending AWS SNS message: ' + (e.fmt if hasattr(e, 'fmt') else '') + ','.join(e.args))
+
+    def get_leads(self, domain, preview):
+        try:
+            dynamodb = boto3.resource('dynamodb',
+                                      region_name=AWS_REGION,
+                                      aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                      aws_session_token=AWS_SESSION_TOKEN)
+            table = dynamodb.Table('gsg-signup-table')
+        except Exception as e:
+            logger.error(
+                'Error connecting to database table: ' + (e.fmt if hasattr(e, 'fmt') else '') + ','.join(e.args))
+            return None
+        expression_attribute_values = {}
+        FilterExpression = []
+        if preview:
+            expression_attribute_values[':p'] = preview
+            FilterExpression.append('preview = :p')
+        if domain:
+            expression_attribute_values[':d'] = '@' + domain
+            FilterExpression.append('contains(email, :d)')
+        if expression_attribute_values and FilterExpression:
+            response = table.scan(
+                FilterExpression=' and '.join(FilterExpression),
+                ExpressionAttributeValues=expression_attribute_values,
+            )
+        else:
+            response = table.scan(
+                ReturnConsumedCapacity='TOTAL',
+            )
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            return response['Items']
+        logger.error('Unknown error retrieving items from database.')
+        return None
+
--- form/views.py
+++ form/views.py
@@ -1,12 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Leads
+from collections import Counter
 
 def home(request):
     return render(request, 'form/index.html')
 
-
 def signup(request):
     leads = Leads()
     status = leads.insert_lead(request.POST['name'], request.POST['email'], request.POST['previewAccess'])
+    if status == 200:
+        leads.send_notification(request.POST['email'])
     return HttpResponse('', status=status)
+def search(request):
+    domain = request.GET.get('domain')
+    preview = request.GET.get('preview')
+    leads = Leads()
+    items = leads.get_leads(domain, preview)
+    if domain or preview:
+        return render(request, 'search.html', {'items': items})
+    else:
+        domain_count = Counter()
+        domain_count.update([item['email'].split('@')[1] for item in items])
+        return render(request, 'search.html', {'domains': sorted(domain_count.items())})
